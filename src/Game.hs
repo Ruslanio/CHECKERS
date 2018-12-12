77dcd559a4e9e0cabea49552786837cbce0b7e51
rmirkhusianov <mirhusainow@gmail.com>
2018-12-12 18:46:03 +0300

    done

--- src/Game.hs
+++ src/Game.hs
@@ -1,6 +1,7 @@
 -- | Модуль, отвечающий за логику игры
 module Game where
-
+import Prelude hiding (Either(..))
+import System.Random
 import Data.Array
 import Util
 
@@ -15,10 +16,69 @@ type Board = Array Pos Label
 
 data Game = Game {
         emptyField  :: Pos,
-        gameBoard   :: Board }
+        gameBoard   :: Board } deriving (Eq)
 
 shuffle :: Int -> IO Game
-shuffle = un
+shuffle n = (iterate (shuffle1 =<<) $ pure initGame) !! n
+
+shuffle1 :: Game -> IO Game
+shuffle1 g = flip move g <$> (randomElem $ nextMoves g)
+
+randomElem :: [a] -> IO a
+randomElem xs = (xs !! ) <$> randomRIO (0, length xs - 1)
+
+nextMoves g = filter (within . moveEmptyTo . orient) allMoves
+    where moveEmptyTo v = shift v (emptyField g)
+          allMoves = [Up, Down, Left, Right]
 
 isGameOver :: Game -> Bool
-isGameOver = un
+isGameOver = ( == initGame)
+
+newtype Vec = Vec (Int, Int)
+
+move :: Move -> Game -> Game
+move m (Game id board)
+    | within id' = Game id' $ board // updates
+    | otherwise  = Game id board
+    where id' = shift (orient m) id
+          updates = [(id, board ! id'), (id', emptyLabel)]
+
+-- определение того, что индексы внутри доски
+within :: Pos -> Bool
+within (a, b) = p a && p b
+    where p x = x >= 0 && x <= 3
+
+-- смещение положение по направлению
+shift :: Vec -> Pos -> Pos
+shift (Vec (va, vb)) (pa, pb) = (va + pa, vb + pb)
+
+-- направление хода
+orient :: Move -> Vec
+orient m = Vec $ case m of
+    Up      -> (-1, 0)
+    Down    -> (1 , 0)
+    Left    -> (0 ,-1)
+    Right   -> (0 , 1)
+
+-- метка для пустой фишки
+emptyLabel :: Label
+emptyLabel = 15
+
+initGame :: Game
+initGame = Game (3, 3) $ listArray ((0, 0), (3, 3)) $ [0 .. 15]
+
+instance Show Game where
+  show (Game _ board) = "\n" ++ space ++ line ++
+          (foldr (\a b -> a ++ space ++ line ++ b) "\n" $ map column [0 .. 3])
+          where post id = showLabel $ board ! id
+                showLabel n  = cell $ show $ case n of
+                          15 -> 0
+                          n  -> n+1
+                cell "0"   = "    "
+                cell [x]   = ' ':' ': x :' ':[]
+                cell [a,b] = ' ': a : b :' ':[]
+                line = "+----+----+----+----+\n"
+                nums = ((space ++ "|") ++ ) . foldr (\a b -> a ++ "|" ++ b) "\n".
+                          map post
+                column i = nums $ map (\x -> (i, x)) [0 .. 3]
+                space = "\t"
--- src/Loop.hs
+++ src/Loop.hs
@@ -1,24 +1,23 @@
 -- | Модуль, отвечающий за передачу и обработку комманд от пользователя
 module Loop where
-
+import Prelude hiding (Either(..))
 import Game
 import Util
+import Data.Char (isDigit)
 
 data Query = Quit | NewGame Int | Play Move
 
 play :: IO ()
 play = greetings >> setup >>= gameLoop
 
-greetings :: IO ()
-greetings = un
+
 
 setup :: IO Game
 setup = putStrLn "Начнём новую игру?" >>
     putStrLn "Укажите сложность (положительное целое число): " >>
     getLine >>= maybe setup shuffle . readInt                      -- | '.' значит что мы вычисляеи байду справа и пихаем то что получилось в байду слева
 
-readInt :: String -> Maybe Int
-readInt = un
+
 
 gameLoop :: Game -> IO ()
 gameLoop game
@@ -26,14 +25,73 @@ gameLoop game
     | otherwise         = showGame game >> askForMove >>= reactOnMove game
 
 
-showResults :: Game -> IO ()
-showResults = un
+remindMoves :: IO ()
+remindMoves = mapM_ putStrLn talk
+    where talk = [
+            "Возможные ходы пустой клетки:",
+            "   left     или l       -- налево",
+            "   right    или r       -- направо",
+            "   up       или u       -- вверх",
+            "   down     или d       -- вниз",
+            "Другие действия:",
+            "   new int  или n int -- начать новую игру, int - целое число,",
+                                      "указывающее на сложность",
+            "   quit     или q      -- выход из игры"]
 
-showGame :: Game -> IO ()
-showGame = un
 
-askForMove :: IO Query
-askForMove = un
+
 
 reactOnMove :: Game -> Query -> IO ()
-reactOnMove = un
+reactOnMove game query = case query of
+    Quit        -> quit
+    NewGame n   -> gameLoop =<< shuffle n
+    Play    m   -> gameLoop $ move m game                           -- | '$' просто убирает скобки (одну там где стоит, вторую на конце строки)
+
+askForMove :: IO Query
+askForMove = showAsk >>
+    getLine >>= maybe askAgain return . parseQuery
+    where askAgain = wrongMove >> askForMove
+
+
+parseQuery :: String -> Maybe Query
+parseQuery x = case x of
+    "up"    -> Just $ Play Up
+    "u"     -> Just $ Play Up
+    "down"  -> Just $ Play Down
+    "d"     -> Just $ Play Down
+    "left"  -> Just $ Play Left
+    "l"     -> Just $ Play Left
+    "right" -> Just $ Play Right
+    "r"     -> Just $ Play Right
+    "quit"  -> Just $ Quit
+    "q"     -> Just $ Quit
+
+    'n':'e':'w':' ':n   -> Just . NewGame =<< readInt n
+    'n':' ':n           -> Just . NewGame =<< readInt n
+    _       -> Nothing
+
+
+readInt :: String -> Maybe Int
+readInt n
+    | all isDigit n = Just $ read n
+    | otherwise     = Nothing
+
+greetings :: IO ()
+greetings = putStrLn "Привет! Это игра пятнашки" >>
+    showGame initGame >>
+    remindMoves
+
+showAsk :: IO ()
+showAsk = putStrLn "Ваш ход: "
+
+showResults :: Game -> IO ()
+showResults g = showGame g >> putStrLn "Игра окончена."
+
+showGame :: Game -> IO ()
+showGame = putStrLn . show
+
+wrongMove :: IO ()
+wrongMove = putStrLn "Не могу распознать ход." >> remindMoves
+
+quit :: IO ()
+quit = putStrLn "До встречи." >> return ()
